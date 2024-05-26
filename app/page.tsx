21bdeed8489f531407adf906f6d2d537d51bed49
bosco0808 <bosco0808@gmail.com>
2024-05-26 13:38:40 +0800

    centered home page + added start button

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
+    <div className={styles.center}>
       <h1>Fitness training app</h1>
-    </>
+      <Link href={"/signup"} className="btn btn-primary">
+        Get started
+      </Link>
+    </div>
   );
 }
