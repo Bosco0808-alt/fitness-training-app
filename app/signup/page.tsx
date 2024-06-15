865d177d074476632dde24498a0da43fe4c2156e
bosco0808 <bosco0808@gmail.com>
2024-06-15 11:48:27 +0800

    added forms in signin and signup pages 🚀

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,5 +1,21 @@
 const Signup = () => {
-  return <h1></h1>;
+  return (
+    <form>
+      <div className="form-group">
+        <label className="m-2">Username: </label>
+        <input className="form-control m-2" autoComplete="username"></input>
+        <label className="m-2">Password: </label>
+        <input
+          className="form-control m-2"
+          type="password"
+          autoComplete="new-password"
+        ></input>
+      </div>
+      <button type="submit" className="btn btn-primary m-2">
+        Sign Up
+      </button>
+    </form>
+  );
 };
 
 export default Signup;
